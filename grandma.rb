13b790eaac19b8d61f09edd5e34bc1d83ebd50c8
Brian Donnoe <dunow33@hotmail.com>
2020-07-30 17:32:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,9 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+  #elsif phrase == "I LOVE YOU GRANDMA!"
+   # return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return
   end
 end
